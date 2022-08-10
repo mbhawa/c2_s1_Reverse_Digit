726efe8369ecce2f1b2b4dd550901d5266d0f979
Mridul <mridulbhawa@gmail.com>
2022-08-10 21:47:08 +0530

    added test code for ReverseDigitTest

--- src/test/java/ReverseDigitTest.java
+++ src/test/java/ReverseDigitTest.java
@@ -1,12 +1,14 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ReverseDigitTest {
-
+    DigitReverse reverse;
 
     @BeforeEach
     void setUp() {
+        reverse = new DigitReverse();
     }
 
     @AfterEach
@@ -14,6 +16,26 @@ class ReverseDigitTest {
     }
 
     @Test
-    void main() {
+    void givenInputPositiveNumberReverseTheDigit() {
+        //arrange
+        int number = 12345678;
+        int expectedValue = 87654321;
+        //act
+        int actualValue = reverse.getReverse(number);
+        //assert
+        Assertions.assertEquals(expectedValue, actualValue, "Please check your code");
+    }
+
+    @Test
+    void givenInputNegativeNumberReverseTheDigit() {
+        //arrange
+        int number = -12345678;
+        int expectedValue = -87654321;
+        //act
+        int actualValue = reverse.getReverse(number);
+        //assert
+        Assertions.assertEquals(expectedValue, actualValue, "Please check your code");
+
     }
 }
+
